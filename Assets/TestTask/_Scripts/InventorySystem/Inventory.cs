9ed13ea57030eba02c42686dc613c27d37093382
PhantomHug <109718968+PhantomHug@users.noreply.github.com>
2025-03-03 12:29:47 +0300

    Refactor inventory. Add ui display changes

--- Assets/TestTask/_Scripts/InventorySystem/Inventory.cs
+++ Assets/TestTask/_Scripts/InventorySystem/Inventory.cs
@@ -10,27 +10,45 @@ namespace TestTask._Scripts.InventorySystem
 {
     public class Inventory : MonoBehaviour, IDataPersistence
     {
+        [Header("")]
+        [Space(5)]
         [SerializeField] private int _maxInventory;
         [SerializeField] private int _availableInventory;
         [SerializeField] private Slot[] _slots;
         
-        private void OnEnable()
+        [Header("Temporary data for unlock slots")]
+        [Space(5)]
+        [SerializeField] private float _money = 99999;
+        
+        [Header("Data persistence settings")]
+        [Space(5)]
+        [Tooltip("Set true if you want load saved data")]
+        [SerializeField] private bool _useLoad;
+        
+        private void AfterLoad()
         {
-            
-/*#if UNITY_EDITOR
-            _slots = new Slot[_maxInventory];
+            var visualSlots = FindObjectsOfType<InventorySlot>();
+            for (int i = _slots.Length - 1; i >= 0; i--)
+            {
+                _slots[i].InitVisual(visualSlots[_slots.Length - i - 1]);
+                _slots[i].UpdateVisual();
+            }
+
             for (int i = 0; i < _availableInventory; i++)
             {
-                _slots[i] = new Slot();
-                _slots[i].Unlock();
-            }      
-#endif*/
+                _slots[i].TryUnlock(ref _money);
+            }
         }
-        
+
         public void UnlockLastSlot()
         {
-            if(_availableInventory <= _maxInventory - 1)
-                _slots[_availableInventory++].Unlock();
+            if (_availableInventory <= _maxInventory - 1)
+            {
+                if (_slots[_availableInventory].TryUnlock(ref _money))
+                {
+                    _availableInventory++;
+                }
+            }
             else
                 Debug.Log("Available inventory size is equal to max inventory size");
         }
@@ -54,7 +72,7 @@ namespace TestTask._Scripts.InventorySystem
         private int TryAddExistingItem(InventoryItem item)
         {
             short resultValue = -1;
-            var allSameSlots = _slots.Where(slot => slot.GetItem().Item == item.Item).ToList();
+            var allSameSlots = _slots.Where(slot => slot.GetInventoryItem().Item == item.Item).ToList();
             foreach (var slot in allSameSlots)
             {
                 resultValue = (short)slot.TryAddItem(item);
@@ -74,7 +92,7 @@ namespace TestTask._Scripts.InventorySystem
 
         }
         
-        public void TryRemoveRandomItem()
+        public void RemoveRandomItem()
         {
             var notEmptySlots = _slots.Where(slot => !slot.IsEmpty).ToList();
             if (notEmptySlots.Count > 0)
@@ -89,57 +107,69 @@ namespace TestTask._Scripts.InventorySystem
         
         public void Shoot()
         {
-            var weaponSlots = GetAllWeaponSlots();
+            var weaponSlots = GetSlotsByType(ItemType.WEAPON);
             if (weaponSlots.Count == 0)
             {
-                Debug.Log("Pew, pew. You shot from your finger. Because you have no any weapon");
+                Debug.Log("You cant shoot because you have no weapon.");
                 return;
             }
 
-            var ammoSlots = GetAllAmmoSlots();
+            var ammoSlots = GetSlotsByType(ItemType.AMMO);
             if (ammoSlots.Count == 0)
             {
-                Debug.Log("CLICK, CLICK. You have no ammo");
+                Debug.Log("You have no ammo");
                 return;
             }
             
             var weaponWithAmmo = new List<Slot>();
             foreach (var ammo in ammoSlots)
             {
-                var wep = weaponSlots.FirstOrDefault(weapon => ((WeaponItem)weapon.GetItem().Item).Ammo == ammo.GetItem().Item);
+                var wep = weaponSlots.FirstOrDefault(weapon => ((WeaponItem)weapon.GetInventoryItem().Item).Ammo == ammo.GetInventoryItem().Item);
                 if(wep != null)
                     weaponWithAmmo.Add(wep);
             }
-            
+
+            if (weaponWithAmmo.Count == 0)
+            {
+                Debug.Log("You dont have ammo for your weapon");
+                return;
+            }
+
             int randomIndex = new Random().Next(weaponWithAmmo.Count);
             ammoSlots.FirstOrDefault(ammo =>
-                ((WeaponItem)weaponWithAmmo[randomIndex].GetItem().Item).Ammo == ammo.GetItem().Item).RemoveItem(1);
+                ((WeaponItem)weaponWithAmmo[randomIndex].GetInventoryItem().Item).Ammo == ammo.GetInventoryItem().Item).RemoveItem(1);
         }
         
-        private List<Slot> GetAllWeaponSlots()
+        private List<Slot> GetSlotsByType(ItemType itemType) 
         {
-            return _slots.Where(slot => !slot.IsEmpty && slot.ItemType == ItemType.WEAPON).Distinct().ToList();
-        }
-        
-        private List<Slot> GetAllAmmoSlots()
-        {
-            return _slots.Where(slot => !slot.IsEmpty && slot.ItemType == ItemType.AMMO).Distinct().ToList();
+            return _slots.Where(slot => !slot.IsEmpty && slot.ItemType == itemType).Distinct().ToList();
         }
 
         public void Load(GameSaveData persistence)
         {
+            if (!_useLoad) return;
+            
             _maxInventory = persistence.maxInventory;
             _availableInventory = persistence.availableInventory;
             _slots = new Slot[_maxInventory];
-            Array.Copy(persistence.slots, _slots, persistence.slots.Length);
-            for (int i = persistence.slots.Length; i < _slots.Length; i++)
-                _slots[i] = new Slot();
+            for (int i = 0; i < _slots.Length; i++)
+            {
+                if (persistence.slots[i] == null)
+                {
+                    _slots[i] = new Slot();
+                }
+                else
+                {
+                    _slots[i] = persistence.slots[i];
+                }
+            }
+
+            AfterLoad();
         }
         
         public void Save(ref GameSaveData persistence)
         {
-            var notEmptySlots = _slots.Where(slot => !slot.IsEmpty).ToArray();
-            persistence.slots = notEmptySlots;
+            persistence.slots = _slots;
             persistence.maxInventory = _maxInventory;
             persistence.availableInventory = _availableInventory;
         }
@@ -148,32 +178,48 @@ namespace TestTask._Scripts.InventorySystem
     [Serializable]
     public class Slot
     {
-        [SerializeField]
+        [SerializeField] private InventoryItem _item;
+        [SerializeField] private float _cost;
         private bool _isLocked = true;
-        [SerializeField]
-        private InventoryItem _item;
+        private InventorySlot _visualSlot;
+        
         public bool IsLocked => _isLocked;
         public bool IsEmpty => _item.Item == null;
         public ItemType ItemType => _item.Item.ItemType;
         
-        public void Unlock()
+        public bool TryUnlock(ref float money)
         {
-            if (_isLocked)
-                _isLocked = false;
+            if (!_isLocked || money <= _cost) return false;
+            money -= _cost;
+            _isLocked = false;
+            _cost = 0;
+            _visualSlot.Unlock();
+            return true;
         }
+
+        public void InitVisual(InventorySlot visualSlot)
+        {
+            _visualSlot = visualSlot;
+        }
+        
         public void AddNewItem(InventoryItem item)
         {
             _item = item;
+            UpdateVisual();
         }
         public ushort TryAddItem(InventoryItem item)
         {
-            return _item.AddItem(item.Count);
+            var result = _item.AddItem(item.Count);
+            _visualSlot.UpdateCount(_item.Count);
+            return result;
         }
         public bool TryRemoveItem()
         {
             if(IsEmpty)
                 return false;
             _item.RemoveItem();
+            _visualSlot.UpdateCount(0);
+            _visualSlot.UpdateIcon(null);
             return true;
         }
         public void RemoveItem(ushort count)
@@ -181,8 +227,22 @@ namespace TestTask._Scripts.InventorySystem
             _item.RemoveCount(count);
             if (_item.Count == 0)
                 TryRemoveItem();
+            UpdateVisual();
         }
-        //Добавить удаление предмета если его количество равно 0
-        public InventoryItem GetItem() => _item;
+        public InventoryItem GetInventoryItem() => _item;
+
+        public void UpdateVisual()
+        {
+            if (_item != null)
+            {
+                _visualSlot.UpdateIcon(IsEmpty ? null : _item.Item.Icon);
+                _visualSlot.UpdateCount(_item.Count);
+            }
+            else
+            {
+                _visualSlot.UpdateIcon(null);
+                _visualSlot.UpdateCount(0);
+            }
+        }
     }
 }
---
+++ Assets/TestTask/_Scripts/InventorySystem/InventorySlot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TestTask._Scripts.InventorySystem
+{
+    public class InventorySlot : MonoBehaviour
+    {
+        [SerializeField] private Text _itemsCount;
+        [SerializeField] private Image _icon;
+        [SerializeField] private Image _locker;
+        
+        public void UpdateCount(ushort count)
+        {
+            _itemsCount.text = count <= 1 ? "" : count.ToString();
+        }
+
+        public void UpdateIcon(Sprite newIcon)
+        {
+            _icon.enabled = newIcon != null;
+            _icon.sprite = newIcon;
+        }
+
+        public void Unlock()
+        {
+            _locker.enabled = false;
+        }
+    }
+}
